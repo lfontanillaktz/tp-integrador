62070081999a4c7f6c47d0858965d06a9f6b08c8
gezdiaz <gastonezequieldiaz@outlook.com>
2018-10-25 23:19:26 -0300

    Prueba con id en la direccion

--- src/main/java/com/kaitzen/controller/PingRestController.java
+++ src/main/java/com/kaitzen/controller/PingRestController.java
@@ -1,6 +1,7 @@
 package com.kaitzen.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class PingRestController {
     @GetMapping("/ping")
     public String ping() {
         return "pong";
+    }
+
+    @GetMapping("/ping/{id}")
+    public String pingId(@PathVariable("id") int id){
+        return "ping id: "+id;
     }
 
 }
