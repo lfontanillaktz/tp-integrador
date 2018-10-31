179331d4860bed031f255190eb0b9c8341d4f699
Leandro <leandro.olivera03@gmail.com>
2018-10-31 16:13:29 -0300

    creo branch

--- src/main/java/com/kaitzen/controller/PingRestController.java
+++ src/main/java/com/kaitzen/controller/PingRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingRestController {
-
+//akjlaksdjsakljd
     @GetMapping("/ping")
     public String ping() {
         return "pong";
