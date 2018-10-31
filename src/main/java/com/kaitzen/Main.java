44c5f8328ebfea8436655582dd9d11b96d339e79
fakundoa <38055250+fakundoa@users.noreply.github.com>
2018-10-31 18:20:53 -0300

    Creación de clase cliente,proyecto y empleado

--- src/main/java/com/kaitzen/Main.java
+++ src/main/java/com/kaitzen/Main.java
@@ -1,16 +1,20 @@
 package com.kaitzen;
 
+import com.kaitzen.model.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.kaitzen"})
+//@ComponentScan(basePackages = {"com.kaitzen"})
 @EnableJpaAuditing
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+        Client c1=new Client("jorge");
+
     }
 }
